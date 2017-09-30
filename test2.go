d5f173441b6b1f93ac701c891d399c9ba547a37d
mrmola <jhnshaughn@gmail.com>
2017-09-30 20:22:20 +0000

    Add files via upload

--- test2.go
+++ test2.go
@@ -6,16 +6,16 @@ import "strings"
 import "math"
 
 func main() {
-   char := [15]string{"a", "b", "c", "d", "e", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+   char := [17]string{" ", " ", "᠎", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "​", " ", " "}
    //charlist
-   var length int = 4;
+   var length int = 2;
    //var pot = math.Pow(float64((len(char)+1)), length)
    //var list []string
    list := make([]string, 0)
    //output array
   // fmt.Println(cap(list))
    //fmt.Println(len(list))
-   z := []string{"potato", "dont change"}
+   z := []string{"potato", "dont change", ""}
    for i := 0; i < len(char); i++ {
       z[0] = char[i]
       //fmt.Println(list)
@@ -26,10 +26,13 @@ func main() {
    for a := 0; a < length; a++ {
       for i := 0; i < int(math.Pow(float64(lengt), float64(a))); i++ {
          for j := 0; j < len(char); j++ {
-         z[0] = char[j]
-         z[1] = list[i]
-//         fmt.Println(list)
-         list = append(list, strings.Join(z, ""))
+            z[0] = char[j]
+            z[1] = list[i]
+//          fmt.Println(list)
+            if a == 1 {
+               z[2] = "lol"
+            }
+            list = append(list, strings.Join(z, ""))
          }
       }
    }
